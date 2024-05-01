b8009289e4e450432126f0b5ad70914c7f6b48de
Gahan Rakholia <gahan94rakh@gmail.com>
2024-05-01 12:15:14 +0530

    refactor: rename var `commits` to `messages`

--- pkg/jira_changelog/generator.go
+++ pkg/jira_changelog/generator.go
@@ -31,14 +31,14 @@ func (c *Generator) Generate(ctx context.Context) *Changelog {
 	return NewChangelog(c.FromRef, c.ToRef, c.RepoURL, issuesByEpic)
 }
 
-func (c *Generator) fetchJiraIssues(commits []messages.Messager) ([]jira.Issue, error) {
-	slog.Debug("Total commit messages", "count", len(commits))
+func (c *Generator) fetchJiraIssues(msgs []messages.Messager) ([]jira.Issue, error) {
+	slog.Debug("Total commit messages", "count", len(msgs))
 
 	jiraIssues := make([]jira.Issue, 0)
-	for _, commit := range commits {
-		issue, err := c.fetchJiraIssue(commit)
+	for _, msg := range msgs {
+		issue, err := c.fetchJiraIssue(msg)
 		if err != nil {
-			slog.Error("error fetching jira issue", "error", err, "commit", commit)
+			slog.Error("error fetching jira issue", "error", err, "message", msg)
 			return nil, err
 		}
 
@@ -48,17 +48,17 @@ func (c *Generator) fetchJiraIssues(commits []messages.Messager) ([]jira.Issue, 
 	return lo.Uniq(jiraIssues), nil
 }
 
-func (c *Generator) fetchJiraIssue(commit messages.Messager) (jira.Issue, error) {
-	issueId := jira.IssueId(commit.Message())
+func (c *Generator) fetchJiraIssue(msg messages.Messager) (jira.Issue, error) {
+	issueId := jira.IssueId(msg.Message())
 	if issueId == "" {
-		slog.Warn("commit message does not contain issue jira id of the project", "commit", commit)
-		return jira.NewIssue("", commit.Message(), "done", ""), nil
+		slog.Warn("text does not contain issue jira id of the project", "message", msg)
+		return jira.NewIssue("", msg.Message(), "done", ""), nil
 	}
 
 	issue, err := c.Client.FetchIssue(string(issueId))
 	if err != nil {
-		slog.Warn("failed to fetch jira issue", "commit", commit)
-		return jira.NewIssue("", commit.Message(), "done", ""), nil
+		slog.Warn("failed to fetch jira issue", "message", msg)
+		return jira.NewIssue("", msg.Message(), "done", ""), nil
 	}
 	return issue, nil
 }
